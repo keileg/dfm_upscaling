BUG: Fix parameters in basis function computation

local_problems.py
@@ -424,7 +424,7 @@ def cell_basis_functions(
                     # Solve and distribute
                     if gb in ilu_map:
                         x, info = spla.gmres(
-                            A=A, b=b, M=ilu_map[gb], restart=500, maxiter=20
+                            A=A, b=b, M=ilu_map[gb], restart=500, maxiter=20, tol=1e-8
                         )
                         if info > 0:
                             raise ValueError(
@@ -507,7 +507,7 @@ def cell_basis_functions(
         basis_sum = np.sum(np.array([b for b in basis_functions.values()]), axis=0)
         for g, _ in assembler.gb:
             dof = assembler._dof_manager.dof_ind(g, discr.cell_variable)
-            assert np.allclose(basis_sum[dof], 1, rtol=1e-3)
+            assert np.allclose(basis_sum[dof], 1, atol=1e-2)
 
         # Check that the mortar fluxes sum to zero for local problems.
         for e, _ in assembler.gb.edges():